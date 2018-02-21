c171bacc258594b7cb669b9a0ee03902586c68ce
Hanio90 <hanio90@gmail.com>
2018-02-21 00:52:55 -0500

    This is my GC coffee shope lab

---
+++ Lab20CoffeeShopWebApp/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab20CoffeeShopWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult WelcomeUser(string FirstName)
+        {
+            ViewBag.Message = $"Welcome to the Grand Circus Coffee Shop, {FirstName}!";
+            return View();
+        }
+    }
+}

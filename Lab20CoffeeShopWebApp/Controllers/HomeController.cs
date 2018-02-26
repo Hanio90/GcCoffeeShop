c6beb3b219e4c5b132e981399d5c15ab70fc418a
Hanio90 <hanio90@gmail.com>
2018-02-25 21:01:09 -0500

    First commit

--- Lab20CoffeeShopWebApp/Controllers/HomeController.cs
+++ Lab20CoffeeShopWebApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lab20CoffeeShopWebApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +11,16 @@ namespace Lab20CoffeeShopWebApp.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+           
+            using (var db = new CoffeeShopDBEntities())
+            {
+                ItemList itemListModel = new ItemList();
+
+                 itemListModel.ItemsList = db.Items.ToList();
+
+                return View(itemListModel);
+            }
+
         }
 
         public ActionResult About()
@@ -30,10 +40,30 @@ namespace Lab20CoffeeShopWebApp.Controllers
         {
             return View();
         }
-        public ActionResult WelcomeUser(string FirstName)
+
+        //public ActionResult WelcomeUser(string FirstName)
+        //{
+        //    ViewBag.Message = $"Welcome to the Grand Circus Coffee Shop, {FirstName}!";
+        //    return View();
+        //}
+
+        public ActionResult WelcomeUser(User newUser) // New customer is an object of Customer 
         {
-            ViewBag.Message = $"Welcome to the Grand Circus Coffee Shop, {FirstName}!";
-            return View();
+            // 1. Create an object from the ORM class
+            CoffeeShopDBEntities MyORM = new CoffeeShopDBEntities(); // you need this object to talk to the Data base
+
+            // 2.Validation
+
+            // 3.Adding the new customer to Customers list using MyORM.
+            MyORM.Users.Add(newUser);
+
+            //4.Save changes
+            MyORM.SaveChanges();
+
+
+            ViewBag.Message = $"Welcome to the Grand Circus Coffee Shop, {newUser.FirstName}!";
+              return View();
         }
+        
     }
 }
---
+++ Lab20CoffeeShopWebApp/Models/ItemList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab20CoffeeShopWebApp.Models
+{
+    public class ItemList
+    {
+
+        public List<Item> ItemsList;
+
+    }
+}
